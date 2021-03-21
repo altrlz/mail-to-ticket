8f7ce3e4139e5ba192aecd56666751a6434e4fef
kentaro420n1987@gmail.com <kentaro420n1987@gmail.com>
2021-03-21 07:56:19 +0000

    Edited Config, Tickets.Tickets, Main, Tickets, settings, Config.Config, Dashboard and Dashboard.Dashboard

--- client_code/Config/Config/__init__.py
+++ client_code/Config/Config/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import ConfigTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Config(ConfigTemplate):
   def __init__(self, **properties):
